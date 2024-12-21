7edc3ed5a83bfd55852cc4b43def2073d4a25380
Alvin033 <wengsiang.ng@gmail.com>
2024-12-21 16:13:50 +0800

    Alvin : Updated controller model and views

---
+++ ClubManagementSystem/Controllers/AdminHomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClubManagementSystem.Controllers;
+
+[Authorize(Roles = "Admin")]
+public class AdminController : Controller
+{
+    public IActionResult AdminHome()
+    {
+        return View();
+    }
+}
---
+++ ClubManagementSystem/Models/Secure.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClubManagementSystem.Models;
+
+#nullable disable warnings
+
+// View Models ----------------------------------------------------------------
+
+public class LoginVM
+{
+    [StringLength(100)]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [StringLength(100, MinimumLength = 5)]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    public bool RememberMe { get; set; }
+}
+
+public class RegisterVM
+{
+    [StringLength(100)]
+    [EmailAddress]
+    [Remote("CheckEmail", "Account", ErrorMessage = "Duplicated {0}.")]
+    public string Email { get; set; }
+
+    [StringLength(100, MinimumLength = 5)]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [StringLength(100, MinimumLength = 5)]
+    [Compare("Password")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm Password")]
+    public string Confirm { get; set; }
+
+    [StringLength(100)]
+    public string Name { get; set; }
+
+    public IFormFile Photo { get; set; }
+}
+
+public class UpdatePasswordVM
+{
+    [StringLength(100, MinimumLength = 5)]
+    [DataType(DataType.Password)]
+    [Display(Name = "Current Password")]
+    public string Current { get; set; }
+
+    [StringLength(100, MinimumLength = 5)]
+    [DataType(DataType.Password)]
+    [Display(Name = "New Password")]
+    public string New { get; set; }
+
+    [StringLength(100, MinimumLength = 5)]
+    [Compare("New")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm Password")]
+    public string Confirm { get; set; }
+}
+
+public class UpdateProfileVM
+{
+    public string? Email { get; set; }
+
+    [StringLength(100)]
+    public string Name { get; set; }
+
+    public string? PhotoURL { get; set; }
+
+    public IFormFile? Photo { get; set; }
+}
+
+public class ResetPasswordVM
+{
+    [StringLength(100)]
+    [EmailAddress]
+    public string Email { get; set; }
+}
+
+public class EmailVM
+{
+    [StringLength(100)]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    public string Subject { get; set; }
+
+    public string Body { get; set; }
+
+    public bool IsBodyHtml { get; set; }
+}
+
+public class EventVM
+{
+    // TODO
+    [DataType(DataType.Date)]
+    public DateOnly Date { get; set; }
+
+    // TODO
+    [DataType(DataType.Time)]
+    public TimeOnly Time { get; set; }
+
+    [StringLength(100)]
+    public string Name { get; set; }
+}
